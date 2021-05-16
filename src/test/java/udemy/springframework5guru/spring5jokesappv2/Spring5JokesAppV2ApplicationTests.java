2d9ece682fb89c221ba47da21ec311c4fb710234
Tony-ATC <anthony.calascibetta@student.intecbrussel.be>
2021-05-17 01:17:04 +0200

    Initial commit

---
+++ src/test/java/udemy/springframework5guru/spring5jokesappv2/Spring5JokesAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package udemy.springframework5guru.spring5jokesappv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesAppV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
